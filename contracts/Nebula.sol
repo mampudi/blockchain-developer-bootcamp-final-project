254c728233ceb60de686db3bbace17bc9638b3e1
mampudi <kgang.moloke@mampudi.com>
2021-10-24 15:39:32 +0200

    Create Nebula smart contract

---
+++ contracts/Nebula.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Nebula {
+  
+  //State variables
+
+  //Events - publicize actions to external listeners
+
+  //functions
+  constructor() public {
+  }
+
+  // Fallback function - Called if other functions don't match call or
+  // sent ether without data
+  // Typically, called when invalid data is sent
+  // Added so ether sent to this contract is reverted if the contract fails
+  // otherwise, the sender's money is transferred to contract
+  function () external payable {
+        revert();
+    }
+
+
+}
