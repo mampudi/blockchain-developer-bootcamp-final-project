6c0f2270ec4bbda4290f12b45e66f69b3619581d
mampudi <kgang.moloke@mampudi.com>
2021-11-08 17:03:22 +0200

    Shorten event messages

--- contracts/Nebula.sol
+++ contracts/Nebula.sol
@@ -28,10 +28,10 @@ import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
   LotteryState public _sState;
 
   //vrf related
-  address _sLinkToken = 0xa36085F69e2889c224210F603D836748e7dC0088;
-  address _sVrfCoordinator = 0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9;
-  bytes32 _sKeyHash = 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4;
-  uint _sChainLinkFee = 0.1 * 10 ** 18; //0.1 LINK
+  address public _sLinkToken = 0xa36085F69e2889c224210F603D836748e7dC0088;
+  address public _sVrfCoordinator = 0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9;
+  bytes32 public _sKeyHash = 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4;
+  uint public _sChainLinkFee = 0.1 * 10 ** 18; //0.1 LINK
 
   address public _sOwner;
   address payable[] public _sCustomers;
@@ -70,7 +70,7 @@ import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
   //partner pays to register
   function registerPartner(address _partner) public payable returns (bool) {
     require(_sPartners[_partner] == false, "The partnet already exists");
-    require(msg.value >= _sPartnerRegistrationFee, "The partner needs to pay a fee to register: 1000 wei");
+    require(msg.value >= _sPartnerRegistrationFee, "Not enough fee paid 1000 wei");
     _sPartners[_partner] = true;
     _sNumberOfPartners++;
     _sBalances[address(this)] += msg.value;
@@ -80,8 +80,8 @@ import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
 
   //partner pays to register a customer
   function registerCustomer(address payable _customer) public payable returns (bool) {
-    require(_sPartners[msg.sender], "The partner needs to be registered to register a customer");
-    require(msg.value >= _sCustomerRegistrationFee, "The partner needs to pay a fee to register a customer: 100 wei");
+    require(_sPartners[msg.sender], "Partner not register");
+    require(msg.value >= _sCustomerRegistrationFee, "Not enough fee paid: 100 wei");
     //require(_sCustomers[_customer] == false, "The customer already exists");
     _sCustomers.push(_customer);
     _sNumberOfCustomers++;
@@ -94,7 +94,7 @@ import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
   /// @notice Deposit ether into bank
   /// @return The balance of the user after the deposit is made
   function deposit(uint _amount) public payable returns (uint) {
-    require(_sPartners[msg.sender], "Partner should be enrolled before they can make deposits");
+    require(_sPartners[msg.sender], "Partner not enrolled");
     _sBalances[msg.sender] += msg.value;
     emit LogDepositMade(msg.sender, _amount);
     return msg.value;
@@ -107,28 +107,28 @@ import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
   function withdraw(uint withdrawAmount) public returns (uint) {
     //console.log("Kgang", _sBalances[msg.sender]);
     require(_sBalances[msg.sender] >= withdrawAmount,"Insufficient funds");
-    payable(msg.sender).transfer(withdrawAmount);
     _sBalances[msg.sender] = _sBalances[msg.sender] - withdrawAmount;
+    payable(msg.sender).transfer(withdrawAmount);
     emit LogWithdrawal(msg.sender, withdrawAmount, withdrawAmount);
     return withdrawAmount;
   } 
 
   function enterRaffle(address payable _customer, uint totalSpent) public{
-    require(totalSpent > 0,"The customer must spend more than 5 dollars");
+    require(totalSpent > 0,"Not enough funds spent");
     _sLotteryCustomers.push(_customer);
     _sNumberOfLotteryCustomers++;
   }
 
   //getting random winner
   function fulfillRandomness(bytes32 /* requestId*/, uint256 randomness) internal override{
-    require(_sLotteryCustomers.length > 0,"There has to be _sCustomers before the lottery starts");
+    require(_sLotteryCustomers.length > 0,"No customer is lottery");
     require(_sState == LotteryState.Open,"The lottery is closed");
     uint luckyWinner = randomness % _sLotteryCustomers.length;
     address payable winner = _sLotteryCustomers[luckyWinner];
     _sRecentWinningCustomer = winner;
     uint winnings = address(this).balance * _sLotteryPercentage/100;
     (bool success,) = winner.call{value: winnings}("");
-    require(success,"Transfer to lottery winner failed");
+    require(success,"Lottery winner transfer failed");
     _sBalances[winner] = _sBalances[winner] - winnings;
     _sRecentWinnings = winnings;
     _sState = LotteryState.Open;
@@ -136,14 +136,14 @@ import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
   }
 
   function requestFulfillRandomnessLocal(uint256 randomness) public {
-    require(_sLotteryCustomers.length > 0,"There has to be enrolled lottery customers before the lottery starts");
+    require(_sLotteryCustomers.length > 0,"No customer is lottery");
     require(_sState == LotteryState.Open,"The lottery is closed");
      uint luckyWinner = randomness % _sLotteryCustomers.length;
      address payable winner = _sLotteryCustomers[luckyWinner];
      _sRecentWinningCustomer = winner;
      uint winnings = address(this).balance * _sLotteryPercentage/100;
      (bool success,) = winner.call{value: winnings}("");
-     require(success,"Transfer to lottery winner failed");
+     require(success,"Lottery winner transfer failed");
      _sBalances[address(this)] = _sBalances[address(this)] - winnings;
      _sBalances[winner] = _sBalances[winner] + winnings;
      _sRecentWinnings = winnings;
@@ -164,7 +164,7 @@ import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
 
   //running the lottery on a schedule
   function performUpkeep(bytes calldata /* performData */) external override {
-    require(_sState == LotteryState.Open,"The lottery needs to be open before it can be closed");
+    require(_sState == LotteryState.Open,"Lottery is closed");
     require((block.timestamp - _sLastUpkeep) > 1 minutes,"1 minute has not passed");
     require(LINK.balanceOf(address(this))>= _sChainLinkFee,"Not enough Link");
     _sState = LotteryState.Calculating;
